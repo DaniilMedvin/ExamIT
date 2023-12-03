0b4030e5c9ba04bb5129905dedc4c16aada04736
DaniilMedvin <150052223+DaniilMedvin@users.noreply.github.com>
2023-12-03 15:08:06 +0200

    1

--- ExamIT/ExamIT.cpp
+++ ExamIT/ExamIT.cpp
@@ -2,11 +2,23 @@
 #include <Windows.h> 
 using namespace std;
 
-int TickCost (int n, int m, int a, int b, int k, int c) {
-    int IndTick = n * m * a;
-    int GroupTick = (n + k - 1) / k * m * c;
-    int UnlimTick = n * b * m;
-    return min(IndTick, min(GroupTick, UnlimTick));
+int minCost(int n, int m, int a, int b, int c, int k) {
+    int minCost = INT_MAX;
+
+    for (int i = 0; i <= n; ++i) {
+        int indTick = i * a;
+        int groupTick = (n - i + k - 1) / k * c;
+        int totalCost = indTick + groupTick;
+        if (i * m >= n) {
+            minCost = min(minCost, totalCost);
+        }
+    }
+
+    int unlimTick = (n + k - 1) / k * c;
+    minCost = min(minCost, unlimTick);
+    minCost = min(minCost, b * n);
+
+    return minCost;
 }
 
 int main() {
@@ -28,7 +40,7 @@ int main() {
     cout << "Введіть вартість групового безлімітного квитка 'для группи людей':";
     cin >> k;
 
-    cout << "Вартість квитків: " << TickCost(n, m, a, b, k, c) << " Грн" << endl;
+    cout << "Вартість квитків: " << minCost(n, m, a, b, k, c) << " Грн" << endl;
 
     return 0;
 }
